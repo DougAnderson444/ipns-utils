dd6b1e1e16f2069e222b995c2986057a532b80b2
Doug A <douganderson444@gmail.com>
2023-05-17 08:10:09 -0300

    re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
-// re-export sub-crates under utils crate
+//! # IPNS Utils
+//! IPNS Utils package is a collection of crates that are re-exported.
+
+/// IPNS [Entry](ipns_entry) utils
 pub use ipns_entry as entry;
+pub use ipns_server as server;
