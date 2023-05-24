c7cf8796af8ec15251a4bdd2d85afed8c58836b4
Doug A <douganderson444@gmail.com>
2023-05-24 12:40:28 -0300

    plugin scaffolding

--- ipns-interop-test/src/lib.rs
+++ ipns-interop-test/src/lib.rs
@@ -13,11 +13,69 @@ pub fn check_go_ipfs() -> FunResult {
 }
 
 #[cfg(test)]
-mod tests {
+mod go_interop_tests {
     use super::*;
 
     #[test]
     fn test_check_go_ipfs() {
         assert_eq!(check_go_ipfs().ok(), Some("ipfs version 0.18.1".to_owned()))
+    }
+}
+
+#[cfg(test)]
+mod plugin_tests {
+    use anyhow::{Error, Result};
+    use extism::Context;
+    use extism::Function;
+    use extism::Plugin;
+    use extism::ValType;
+    use extism::{CurrentPlugin, UserData, Val};
+    use ipns_plugin_interface::Output;
+
+    /// Host function that is called by the plugin
+    // https://github.com/extism/rust-pdk/blob/main/examples/host_function.rs
+    pub fn hello_world(
+        _plugin: &mut CurrentPlugin,
+        inputs: &[Val],
+        outputs: &mut [Val],
+        _user_data: UserData,
+    ) -> Result<(), Error> {
+        eprintln!("This is a host function!");
+        outputs[0] = inputs[0].clone();
+        Ok(())
+    }
+
+    #[test]
+    fn it_works() -> Result<()> {
+        let wasm = include_bytes!("../../target/wasm32-wasi/release/ipns_plugin_bindings.wasm");
+
+        let context = Context::new();
+        let f = Function::new(
+            "hello_world",
+            [ValType::I64],
+            [ValType::I64],
+            None,
+            hello_world,
+        );
+        let thing = "this".to_string();
+        let mut config = std::collections::BTreeMap::new();
+        config.insert("thing".to_string(), Some(thing.to_owned()));
+
+        let wasi = true;
+        let mut plugin = Plugin::new(&context, wasm, [f], wasi)?.with_config(&config)?;
+
+        let data = plugin.call("count_vowels", "this is a test").unwrap();
+
+        // convert bytes to string
+        let data_str = std::str::from_utf8(data).unwrap();
+        eprintln!("{data_str:?}");
+
+        let d: Output = serde_json::from_str(data_str).unwrap();
+
+        assert_eq!(d.count, 4);
+        assert_eq!(d.config, thing);
+        assert_eq!(d.a, "this is var a!");
+
+        Ok(())
     }
 }
---
+++ ipns-plugin-bindings/README.md
@@ -0,0 +1,7 @@
+# Plugin
+
+Builds IPNS as a Extism plugin.
+
+## Test
+
+Run the `ipns-interop-test` to test the plugin.
